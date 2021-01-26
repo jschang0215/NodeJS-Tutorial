732376d9b6c99b158eddf0cbd205254e097cd8e1
JSChang <2002chang@gmail.com>
2021-01-26 23:54:59 +0900

    Update

--- main.js
+++ main.js
@@ -1,173 +1,153 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-function templateHTML(title, list, body) {
-    return `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>JSChang - ${title}</title>
-            <meta charset="utf-8">
-            <style>
-            body {
-                background-color: white;
-                color: black;
-            }
-            h1 {
-                font-size: 40px;
-                text-align: center;
-                border-bottom: 1px solid light;
-                margin: 0px;
-                padding: 10px;
-            }
-            .saw {
-                color:gray;
-            }
-            .cur {
-                text-decoration: underline;
-            }
-            #grid ol {
-                border-right: 1px solid gray;
-                width: 100px;
-                margin: 0px;
-                padding: 10px;
-                padding-left: 30px;
-            }
-            #grid {
-                display: grid;
-                grid-template-columns: 150px 1fr;
-            }
-            #article {
-                padding: 10px;
-                padding-left: 30px;
-            }
-            #grid h3 {
-                margin: 0px;
-                margin-bottom: 10px;
-            }
-            #grid ul {
-                margin: 0px;
-                padding-left: 30px;
-            }
-            a {
-                color:black;
-                text-decoration: none;
-            }
-            @media(max-width: 800px) {
-                #grid {
-                    display: block;
-                }
-                #grid ol {
-                    border-right: none;
-                }
-                h1 {
-                    border-bottom: none;
-                }
-            }
-            </style>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script>
-            var Body = {
-                setBodyColor: function setBodyColor(color) {
-                    document.querySelector('body').style.color = color;
-                },
-                setBodyBackgroundColor: function setBodyColor(color) {
-                    document.querySelector('body').style.backgroundColor = color;
-                }
-            };
-            
-            var Link = {
-                setLinkColor: function setLinkColor(color) {
-                    // var links = document.querySelectorAll('a');
-                    // for(var i=0; i<links.length; i++) {
-                    //     links[i].style.color = color;
-                    // }
-                    $('a').css("color", color);
-                }
-            };
-            
-            function nightDayHandler(self) {
-                if(self.value==='Night') {
-                    Body.setBodyBackgroundColor('black');
-                    Body.setBodyColor('white');
-                    Link.setLinkColor('white');
-                    document.getElementById('night_day').value = 'Day';
-                } else {
-                    Body.setBodyBackgroundColor('white');
-                    Body.setBodyColor('black');
-                    Link.setLinkColor('black')
-                    document.getElementById('night_day').value = 'Night';
-                }
-            }
-            </script>
-        </head>
-        <body>
-            <h1><a href="/">JSChang</a></h1>
-            <input type="button" id="night_day" value="Night" onclick="nightDayHandler(this)">
-            <div id="grid">
-                ${list}
-                <div id="article">
-                ${body}
-                </div>
-            </div>
-        </body>
-    </html>
-    `
-}
-
-function templatList(filelist, title) {
-    var list = '<ol>'
-    for(var i=0; i<filelist.length; i++) {
-        if(filelist[i] === title) {
-            list += `<li><a href="/?id=${filelist[i]}" class="cur">${filelist[i]}</a></li>`;
-        } else {
-            list += `<li><a href="/?id=${filelist[i]}" class="saw">${filelist[i]}</a></li>`;
-        }
-    }
-    list +='</ol>'
-    return list;
-}
+var qs = require('querystring');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/') {
         if(queryData.id === undefined) {
             fs.readdir('./data', function(error, filelist) {
-                var list = templatList(filelist, title);
-
+                var list = template.list(filelist, queryData.id);
                 title = 'JSChang';
-                var description = 'Hello World.';
-                var template = templateHTML(title, list, `
+                var description = 'Welcome to JSChang homepage';
+                var html = template.HTML(title, list, `
                 <h3>${title}</h3>
                 <ul>
                     ${description}
                 </ul>
+                `, `
+                <a href="/create">Create</a>
                 `);
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             });
         } else {
             fs.readdir('./data', function(error, filelist) {
-                var list = templatList(filelist, title);
-
-                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-                    var template = templateHTML(title, list, `
-                    <h3>${title}</h3>
+                var title = queryData.id;
+                var sanitizedTitle = sanitizeHtml(title);
+                var list = template.list(filelist, sanitizedTitle);
+                var filteredId = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+                    var sanitizedDescription = sanitizeHtml(description);
+                    var html = template.HTML(sanitizedTitle, list, `
+                    <h3>${sanitizedTitle}</h3>
                     <ul>
-                        ${description}
+                        ${sanitizedDescription}
                     </ul>
+                    `, `
+                    <a href="/update?id=${sanitizedTitle}">Update</a>
+                    <form action="/delete_process" method="post" onclick="alert('Do you really want to delete?')">
+                    <input type="hidden" name="id" value="${sanitizedTitle}">
+                    <input type="submit" value="Delete">
+                    </form>
                     `);
                     response.writeHead(200);
-                    response.end(template);
+                    response.end(html);
                 });
             });
         }
+    } else if(pathname === '/create') {
+        fs.readdir('./data', function(error, filelist) {
+            var list = template.list(filelist, title);
+            var title = 'Create';
+            var html = template.HTML(title, list, `
+            <h3>${title}</h3>
+            <ul>
+                <form action="/create_process" method="post">
+                    <p><input type="text" name="title" placeholder="title"></p>
+                    <p><textarea name="description" placeholder="description"></textarea></p>
+                    <p><input type="submit"></p>
+                </form>
+            </ul>
+            `, ` `);
+            response.writeHead(200);
+            response.end(html);
+        });
+    } else if(pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body += data;
+            // Security
+            if(body.length>1e8) {
+                request.connection.destroy();
+            }
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var title = post.title;
+            var sanitizedTitle = sanitizeHtml(title);
+            var description = post.description;
+            var sanitizedDescription = sanitizeHtml(description);
+            fs.writeFile(`data/${sanitizedTitle}`, sanitizedDescription, 'utf8', function(err) {
+                response.writeHead(302, {Location: `/?id=${sanitizedTitle}`});
+                response.end('Succcss');
+            });
+        });
+    } else if(pathname === '/update') {
+        fs.readdir('./data', function(error, filelist) {
+            var filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+                var list = template.list(filelist, title);
+                var title = queryData.id;
+                console.log(description);
+                var html = template.HTML(title, list, `
+                <h3>${title} - update</h3>
+                <ul>
+                    <form action="/update_process" method="post">
+                        <p><input type="hidden" name="id" value="${title}"></p>
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p><textarea name="description" placeholder="description">${description}</textarea></p>
+                        <p><input type="submit"></p>
+                    </form>
+                </ul>
+                `, ` `);
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+    } else if(pathname === '/update_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body += data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var sanitizedTitle = sanitizeHtml(title);
+            var description = post.description;
+            var sanitizedDescription = sanitizeHtml(description);
+            var filteredId = path.parse(id).base;
+            fs.rename(`data/${filteredId}`, `data/${sanitizedTitle}`, function(err) {
+                fs.writeFile(`data/${sanitizedTitle}`, sanitizedDescription, 'utf8', function(err) {
+                    response.writeHead(302, {Location: `/?id=${sanitizedTitle}`});
+                    response.end('Succcss');
+                });
+            })
+        });
+    } else if(pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body += data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            var filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, function(err) {
+                response.writeHead(302, {Location: `/`});
+                response.end('Succcss');
+            });
+        });
     } else {
+        console.log(_url);
         response.writeHead(404);
         response.end('Page Not Found');
     }
